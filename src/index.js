28a6c0165293bb3cd72515a29ac6a248d5738e38
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-05-13 17:52:52 -0300

    Add antd styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import PlacesAutocompleteAdvanced from './places-autocomplete/advanced'
 import PlacesAutocompleteSimple from './places-autocomplete/simple'
 
+import 'antd/dist/antd.css'
+
 export {
   PlacesAutocompleteAdvanced,
   PlacesAutocompleteSimple
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, include: path.resolve("src"), loader: "babel-loader" }
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   },
   externals: {
