37bfe782d6725c73f7e994bd253d0312b3474f6c
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-06-16 18:30:05 -0300

    Only use required styles for antd component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import PlacesAutocompleteAdvanced from './places-autocomplete/advanced'
 import PlacesAutocompleteSimple from './places-autocomplete/simple'
 
+import 'antd/lib/auto-complete/style/index.less'
+import 'antd/lib/button/style/index.less'
+import 'antd/lib/input/style/index.less'
+import 'antd/lib/select/style/index.less'
+
 export {
   PlacesAutocompleteAdvanced,
   PlacesAutocompleteSimple
